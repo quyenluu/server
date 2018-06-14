efdd71c4a99e78ae733b1bcb56f6dd8b3ee837ee
quyen <quyenluu1989@gmail.com>
2018-06-14 14:46:11 +0700

    update login.php

--- login.php
+++ login.php
@@ -1,17 +1,23 @@
 <?php
-if($_POST["email"] == 'admin@gmail.com'){
-        echo json_encode(array(
-                "success" => true,
-                "userToken" => "1234567890",
-                "dataUser" => array(
-                        "fullName" => "Quyen Luu",
-                        "phone" => "0987654321"
-                )
-        ));
-}else{
-        echo json_encode(array(
-                "success" => false,
-                "message" => "Username or Password invalid",
-                "email" => $_POST["email"]
-        ));
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "POST"){
+	$json = file_get_contents("php://input");
+	$input = json_decode($json, true);
+	if($input["email"] == 'admin@gmail.com'){
+                echo json_encode(array(
+                        "success" => true,
+                        "userToken" => "1234567890",
+                        "dataUser" => array(
+                                "fullName" => "Quyen Luu",
+                                "phone" => "0987654321"
+                        )
+                ));
+        }else{
+                echo json_encode(array(
+                        "success" => false,
+                        "message" => "Username or Password invalid",
+                        "email" => $input["email"]
+                ));
+        }
+	
 }
